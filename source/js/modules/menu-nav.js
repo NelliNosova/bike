f61103817af417f69e28a72f6ac859e975c69eff
Nelli <nellinosovapl@gmail.com>
2021-02-25 21:52:14 +0100

    fixed mentor's remarks

--- source/js/modules/menu-nav.js
+++ source/js/modules/menu-nav.js
@@ -1,21 +1,25 @@
+import {disableScrolling, enableScrolling} from '../utils/scroll-lock';
+
 const header = document.querySelector('.js-header');
-const mainNav = document.querySelector('.js__main-nav');
-const menuBtn = mainNav.querySelector('.js__menu-toggle');
+const mainNav = document.querySelector('.js-main-nav');
+const menuBtn = mainNav.querySelector('.js-menu-toggle');
 const menuItems = mainNav.querySelectorAll('.js__menu-item');
 
 const toggleMenu = () => {
   const closeMenu = () => {
-    header.style.position = 'absolute';
+    header.classList.add('header--absolute');
     mainNav.classList.remove('main-nav--open');
     mainNav.classList.add('main-nav--close');
+    enableScrolling();
   };
 
   const openMenu = () => {
     mainNav.classList.remove('main-nav--close');
     mainNav.classList.add('main-nav--open');
+    disableScrolling();
   };
 
-  const openCloseMenu = () => {
+  const switchMenu = () => {
     if (mainNav.classList.contains('main-nav--close')) {
       openMenu();
     } else if (mainNav.classList.contains('main-nav--open')) {
@@ -23,11 +27,27 @@ const toggleMenu = () => {
     }
   };
 
+  const anchors = document.querySelectorAll('a[href*="#"]');
+
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', (evt) => {
+      evt.preventDefault();
+
+      const linkID = anchor.getAttribute('href').substr(1);
+      const blockID = document.getElementById(linkID);
+
+      blockID.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  }
+
   closeMenu();
-  menuBtn.addEventListener('click', openCloseMenu);
+  menuBtn.addEventListener('click', switchMenu);
 
   menuItems.forEach((item) => {
-    item.addEventListener('click', openCloseMenu);
+    item.addEventListener('click', switchMenu);
   });
 };
 
